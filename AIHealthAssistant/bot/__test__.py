d9a25a6fc094bd801c0ccf0bd54720eef1817f47
Daniil Timachov <danyatimachov@gmail.com>
2021-03-20 00:52:24 +0200

    👌 IMPROVE: Adding name query handling

--- AIHealthAssistant/bot/__test__.py
+++ AIHealthAssistant/bot/__test__.py
@@ -1,9 +1,4 @@
 from bot import bot
-from database import User, db
 
 if __name__ == "__main__":
-    User.create(user_id=123)
-    # db.connect()
-    # db.create_tables([User])
-    # print(user)
-    # bot.polling(none_stop=True)
+    bot.polling(none_stop=True)
--- AIHealthAssistant/bot/bot.py
+++ AIHealthAssistant/bot/bot.py
@@ -7,6 +7,8 @@ from dotenv import load_dotenv, find_dotenv
 from state import State
 from utils import Loader
 
+from database import User
+
 load_dotenv(find_dotenv(), verbose=True)
 
 bot = TeleBot(os.getenv("BOT_TOKEN"))
@@ -16,9 +18,26 @@ responses = Loader.load_responses()
 
 
 @bot.message_handler(commands=['start'])
-def start(msg: types.Message):
+def handle_start(msg: types.Message):
     bot.send_message(
         msg.from_user.id,
         responses['start'][locale]
     )
-    # set users state to State.NAME_INPUT
+    User.create(
+        user_id=msg.from_user.id,
+        state=State.NAME_INPUT
+    )
+
+
+@bot.message_handler(func=lambda msg: User.get_state(msg.from_user))
+def handle_name_input(msg: types.Message):
+    new_name = msg.text
+    User.update_name(msg.from_user, new_name)
+
+    bot.send_message(
+        msg.from_user.id,
+        responses['welcome'][locale].format(new_name),
+        parse_mode='Markdown'
+    )
+
+    User.update_state(msg.from_user, State.NEUTRAL)
